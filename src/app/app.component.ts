69adb98f936dbea954b683959ccf00f48470f950
aleynaflo <selekaleyna@gmail.com>
2024-08-20 05:30:28 +0300

    Component Selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  // selector: '.app-root',
+  // selector: '#app-root',
+  selector: 'app',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ToDoApp';
+  title = 'ToDo Application';
 }
